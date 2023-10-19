f7e200d2fc8561ab60f33a795e44e47cc798816a
jelxn6 <jelxn6@umsystem.edu>
2023-10-19 13:38:38 -0500

    Adding number and go assets for the level countdown issue #3, as well as a fading animation between numbers displayed.

--- visualization/src/main/kotlin/com/enigmashowdown/visual/LevelCountdown.kt
+++ visualization/src/main/kotlin/com/enigmashowdown/visual/LevelCountdown.kt
@@ -1,8 +1,11 @@
 package com.enigmashowdown.visual
 
+import com.badlogic.gdx.scenes.scene2d.Actor
 import com.badlogic.gdx.scenes.scene2d.Stage
+import com.badlogic.gdx.scenes.scene2d.ui.Image
 import com.badlogic.gdx.utils.Scaling
 import com.badlogic.gdx.utils.viewport.ScalingViewport
+import com.enigmashowdown.EnigmaShowdownConstants
 import com.enigmashowdown.game.conquest.state.ConquestStateView
 import com.enigmashowdown.message.broadcast.LevelStateBroadcast
 import com.enigmashowdown.util.getLogger
@@ -22,6 +25,10 @@ class LevelCountdown(
 
     val renderable: Renderable
     private val stage: Stage
+    private val three: Actor
+    private val two: Actor
+    private val one: Actor
+    private val go: Actor
     // maybe declare some images here
 
     init {
@@ -30,9 +37,28 @@ class LevelCountdown(
         // Notice the world width and world height, these values don't matter since this stage is only for the countdown,
         //   but some values are relative to the world width and height, such as the call to setBounds() below
         stage = Stage(ScalingViewport(Scaling.fit, 10f, 10f), renderObject.batch)
-//        stage.addActor(Image(renderObject.mainSkin.getDrawable("countdown/3")).apply {
-//            setBounds(-.5f, -.5f, 1f, 1f)
-//        })
+
+        three = Image(renderObject.mainSkin.getDrawable("3")).apply {
+            setBounds(4f, 4f, 2f, 2f)
+        }
+        two = Image(renderObject.mainSkin.getDrawable("2")).apply {
+            setBounds(4f, 4f, 2f, 2f)
+        }
+        one = Image(renderObject.mainSkin.getDrawable("1")).apply {
+            setBounds(4f, 4f, 2f, 2f)
+        }
+        go = Image(renderObject.mainSkin.getDrawable("go")).apply {
+            setBounds(3f, 4f, 4f, 2f)
+        }
+        three.color.a = 0f
+        two.color.a = 0f
+        one.color.a = 0f
+        go.color.a = 0f
+        stage.addActor(three)
+        stage.addActor(two)
+        stage.addActor(one)
+        stage.addActor(go)
+
         renderable = RenderableMultiplexer(
             listOf(
                 StageRenderable(stage),
@@ -48,6 +74,31 @@ class LevelCountdown(
 
         if (previousState.ticksUntilBegin > 0) {
             logger.info("Ticks until begin: {}", previousState.ticksUntilBegin)
+        }
+
+        if (previousState.ticksUntilBegin > 4 * EnigmaShowdownConstants.TICKS_PER_SECOND) {
+            // Resetting all countdown assets to be transparent for ticks 50-40
+            three.color.a = 0f
+            two.color.a = 0f
+            one.color.a = 0f
+            go.color.a = 0f
+        } else if (previousState.ticksUntilBegin > 3 * EnigmaShowdownConstants.TICKS_PER_SECOND) {
+            // Display number 3 from ticks 40-30, with the alpha channel going from 1.0-0.0 in the 10 ticks
+            three.color.a = ((previousState.ticksUntilBegin - percent) % EnigmaShowdownConstants.TICKS_PER_SECOND) / EnigmaShowdownConstants.TICKS_PER_SECOND.toFloat()
+        } else if (previousState.ticksUntilBegin > 2 * EnigmaShowdownConstants.TICKS_PER_SECOND) {
+            // Display number 2 from ticks 30-20, with the alpha channel going from 1.0-0.0 in the 10 ticks
+            three.color.a = 0f
+            two.color.a = ((previousState.ticksUntilBegin - percent) % EnigmaShowdownConstants.TICKS_PER_SECOND).toFloat() / EnigmaShowdownConstants.TICKS_PER_SECOND.toFloat()
+        } else if (previousState.ticksUntilBegin > 1 * EnigmaShowdownConstants.TICKS_PER_SECOND) {
+            // Display number 1 from ticks 20-10, with the alpha channel going from 1.0-0.0 in the 10 ticks
+            two.color.a = 0f
+            one.color.a = ((previousState.ticksUntilBegin - percent) % EnigmaShowdownConstants.TICKS_PER_SECOND).toFloat() / EnigmaShowdownConstants.TICKS_PER_SECOND.toFloat()
+        } else if (previousState.ticksUntilBegin > 0 * EnigmaShowdownConstants.TICKS_PER_SECOND) {
+            // Display GO! from ticks 10-0, with the alpha channel going from 1.0-0.0 in the 10 ticks
+            one.color.a = 0f
+            go.color.a = ((previousState.ticksUntilBegin - percent) % EnigmaShowdownConstants.TICKS_PER_SECOND).toFloat() / EnigmaShowdownConstants.TICKS_PER_SECOND.toFloat()
+        } else {
+            go.color.a = 0f
         }
 
         previousState.ticksUntilBegin // You can access this, which tells you how many ticks until the level begins.
