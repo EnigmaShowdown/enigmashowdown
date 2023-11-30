f1634fad631b886430806c2d104d59fab4e7e2a1
zssxyh <tardisdoctor999@gmail.com>
2023-11-30 17:21:49 -0600

    next

--- test-ai/src/main/kotlin/com/enigmashowdown/ai/RadianAI.kt
+++ test-ai/src/main/kotlin/com/enigmashowdown/ai/RadianAI.kt
@@ -12,7 +12,7 @@ import com.enigmashowdown.message.request.PlayerActionRequest
 import com.enigmashowdown.util.getLogger
 import java.util.UUID
 
-object MinimalMoveAI : StateListener {
+object RadianAI : StateListener {
     override fun onBroadcast(playerId: UUID, levelStateBroadcast: LevelStateBroadcast, client: RequestClient) {
         val state = levelStateBroadcast.gameStateView as ConquestStateView
         val player = state.entities.firstOrNull { it.id == playerId } ?: error("player not found")
@@ -22,9 +22,29 @@ object MinimalMoveAI : StateListener {
             PlayerActionRequest(
                 playerId,
                 state.tick,
-                ConquestAction(
-                   moveAction = MoveAction.fromDegrees(30.0, 5.0),
-                ),
+
+                if (state.tick < 50.0)
+                {
+                    ConquestAction(
+                        moveAction = MoveAction(Math.toRadians(30.0), 5.0)
+                    )
+                } else if (state.tick < 100.0) {
+                    ConquestAction(
+                        moveAction = MoveAction(Math.toRadians(90.0), 5.0)
+                    )
+                } else if (state.tick < 130.0) {
+                    ConquestAction(
+                        moveAction = MoveAction(Math.toRadians(180.0), 5.0)
+                    )
+                } else if (state.tick < 160.0) {
+                    ConquestAction(
+                        moveAction = MoveAction(Math.toRadians(270.0), 5.0)
+                    )
+                } else {
+                    ConquestAction(
+                        moveAction = MoveAction(Math.toRadians(180.0), 5.0)
+                    )
+                }
             ),
         )
     }
