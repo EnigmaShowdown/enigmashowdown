07350337525e7d211fe942c9488650bd69c5cc75
Lavender Shannon <retrodaredevil@gmail.com>
2023-12-04 00:30:22 -0600

    Formatting

--- visualization/src/main/kotlin/com/enigmashowdown/visual/LevelVisualization.kt
+++ visualization/src/main/kotlin/com/enigmashowdown/visual/LevelVisualization.kt
@@ -6,7 +6,6 @@ import com.badlogic.gdx.graphics.OrthographicCamera
 import com.badlogic.gdx.math.Vector2
 import com.badlogic.gdx.scenes.scene2d.Stage
 import com.badlogic.gdx.utils.viewport.ExtendViewport
-import com.badlogic.gdx.utils.viewport.Viewport
 import com.enigmashowdown.game.conquest.map.LevelMap
 import com.enigmashowdown.game.conquest.state.ConquestStateView
 import com.enigmashowdown.game.conquest.state.EntityType
